3368bd7142836614daa933c2d0c5ed0c34ef64f5
hibreez <4ngus.whitehead@gmail.com>
2015-08-18 10:23:42 +0800

    first commit - basic main.js, package.json and serialport rebuilt for OSX

---
+++ main.js
@@ -0,0 +1,32 @@
+var app = require('app'); 
+var serial = require('serialport');
+var BrowserWindow = require('browser-window'); 
+
+// global reference of window object so it isn't JavaScript GCed.
+var mainWindow = null;
+
+// method called when Electron has initialized.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1250, 
+    height: 1000
+  });
+
+  //load the index.html of the app.
+  mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
