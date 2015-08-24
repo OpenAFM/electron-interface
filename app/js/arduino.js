2d3a232d112ddc336ce7c9ed61135f1762c10517
hibreez <4ngus.whitehead@gmail.com>
2015-08-24 17:56:10 +0800

    altered arduino comms

--- app/js/arduino.js
+++ app/js/arduino.js
@@ -1,11 +1,11 @@
-var serialPort = require("serialport");
-var SerialPort = require("serialport").SerialPort;
+var serialPort = require('serialport');
+var SerialPort = require('serialport').SerialPort;
 var pManager = require('./js/p_session_manager.js');
 var arduino;
 var connection;
 var COM;
 var current_session;
-
+var DONE;
 
 function findBoard(cb) {
   var last = false;
@@ -18,10 +18,10 @@ function findBoard(cb) {
  
       COM = port.comName;
       // check to see if arduino plugged in and open connection
-      if ((COM.search("cu.usbmodem") != -1) ||
-          (COM.search("cu.wchusbserial1410") != -1) ||
-          (COM.search("tty.usbmodem") != -1) || 
-          (COM.search("COM")) != -1) {
+      if ((COM.search('cu.usbmodem') != -1) ||
+          (COM.search('cu.wchusbserial1410') != -1) ||
+          (COM.search('tty.usbmodem') != -1) || 
+          (COM.search('COM')) != -1) {
         arduino = port;
         connection = new SerialPort(arduino.comName, {
           baudrate: 9600
@@ -29,15 +29,15 @@ function findBoard(cb) {
 
         connection.open(function (error) {
           if ( error ) {
-            console.log("failed to open: "+ error);
+            console.log('failed to open: '+ error);
           } else {
-            console.log("Arduino Ready");
+            console.log('Arduino Ready');
             cb(true);
           }
         });
       } else {
         if (last === true){
-          console.log("Arduino not found.");
+          console.log('Arduino not found.');
           cb(false);
         }
       }
@@ -55,10 +55,10 @@ function checkBoard(cb) {
       }
       COM = port.comName;
       // check to see if arduino plugged in and open connection
-      if ((COM.search("cu.usbmodem") != -1) ||
-          (COM.search("cu.wchusbserial1410") != -1) ||
-          (COM.search("tty.usbmodem") != -1) || 
-          (COM.search("COM")) != -1) {
+      if ((COM.search('cu.usbmodem') != -1) ||
+          (COM.search('cu.wchusbserial141') != -1) ||
+          (COM.search('tty.usbmodem') != -1) || 
+          (COM.search('COM')) != -1) {
         cb(true);
       } else {
         if (last === true) {
@@ -72,23 +72,30 @@ function checkBoard(cb) {
 function initialiseBoard(cb) {
   confirmReady(function(){
     console.log('Device Initialised');
-  }, cb);
+  });
 }
       
 function scanProfilo(name) {
+  DONE = false;
   var session = pManger.newSession(name);
   currentSession = session;
   connection.on('data', function(data) {
     data = "" + data;
-    if (data != 'RDY;') {
+    if (data != 'RDY') {
       saveProfilo(data, function() {
-        confirmReady(function(){
-          console.log('Data Received');
-          if (currentSession.data.length == 512 * 256) {
-            endProfilo();
+        console.log('Data Received');
+        if (DONE == true) {
+          endProfilo();
+        }
+        //if this in penultimate line
+        if (currentSession.data.length == 512 * 255) {
             connection.write('DONE;');
-          }
-       });
+            DONE = true;
+        } else {
+          confirmReady(function() {
+            return;
+          });
+        }
       });   
     }
   });
@@ -97,21 +104,21 @@ function scanProfilo(name) {
 function saveProfilo(data, cb) {
   var dataArray = data.split(',');
   if (dataArray.length == 513) {
-    console.log('Data Length Correct');
+    console.log('Data length correct!');
     // remove the semi colon at the end
     dataArray = dataArray.slice(0, dataArray.length() - 1);
     dataArray.forEach(function(point) {
       currentSession.data.push(point);
     }, cb);
   } else {
-    console.log('Error: Data Length Incorrect!');
+    console.log('Error: Data length incorrect, cancelling scan!', endProfilo);
   }
 }
 
 function confirmReady(cb) {
   connection.write('RDY;', function(){
     connection.on('data', function(data) {
-      if (data == 'RDY;') {
+      if (data == 'RDY') {
         cb();
       } else {
         console.log('Error: Did not receive ready confirmation'); 
